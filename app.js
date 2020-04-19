e7c55f2b1a331153a18f1a71965fe229df38e1d1
Matthew Southcott <mjsouthcott@gmail.com>
2020-04-19 10:47:48 -0400

    Add ability to render HTML and write to file

--- app.js
+++ app.js
@@ -4,34 +4,14 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const writeFileP = util.promisify(fs.writeFile);
 
 const team = [];
 
@@ -82,7 +62,7 @@ function validateGitHub(GitHub) {
 }
 
 function validateSchool(school) {
-  const re = /^[a-zA-Z\d_-]{2,}$/;
+  const re = /^[a-zA-Z\d_- ]{2,}$/;
   if (re.test(school)) {
     return true;
   } else {
@@ -90,7 +70,7 @@ function validateSchool(school) {
   }
 }
 
-// Define title case function
+// Define function to converts strings to title case
 function toTitleCase(string) {
   const wordArr = [];
   for (const word of string.toLowerCase().split(" ")) {
@@ -203,8 +183,16 @@ async function createTeam() {
     }
   }
 
-  // Test
-  console.log(team);
+  // Render HTML
+  const teamHTML = render(team);
+
+  // Write HTML to file
+  writeFileP(outputPath, teamHTML, function (err) {
+    if (err) {
+      throw err;
+    }
+  });
 }
 
+// Call function to create team
 createTeam();
